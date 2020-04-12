f08599391276e9fdf25da1c1921e2ce48b1bd4ea
moshe <7moshe7@gmail.com>
2020-04-13 00:33:33 +0300

    Updates

---
+++ precache-manifest.bcc4902d6f69cb5f7ba0921ec87be5ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7667b8f2b0ad7a9ab35a68ebc2227724",
+    "url": "/simple-todo-app-react/index.html"
+  },
+  {
+    "revision": "8773bc65c59de37eac05",
+    "url": "/simple-todo-app-react/static/css/main.26bdc978.chunk.css"
+  },
+  {
+    "revision": "29e21f22a432d73de962",
+    "url": "/simple-todo-app-react/static/js/2.d3cdb963.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app-react/static/js/2.d3cdb963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8773bc65c59de37eac05",
+    "url": "/simple-todo-app-react/static/js/main.fefaacc9.chunk.js"
+  },
+  {
+    "revision": "326b20fb63ff942d3485",
+    "url": "/simple-todo-app-react/static/js/runtime-main.f5242ebe.js"
+  }
+]);
